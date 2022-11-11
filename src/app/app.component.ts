9a6ca0e54bb015c47745930b9f648b5532f61efa
Christofer <christofer.guimaraes@projuris.com.br>
2022-11-11 00:26:38 -0300

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'state-project';
+
+  counter = 0;
+
+  soma = 0;
+
+  setContador(event: string) {
+    this.counter++;
+    this.soma = Number(event)+this.counter;
+
+    return this.soma;
+  }
 }
